c698802fa1121d6edaae5f2a2164f6274975a271
Vadim Khublarian <vadimjedi@gmail.com>
2016-09-04 21:31:22 +0300

    Initial commit

---
+++ src/com/masterjedi/DooBee.java
@@ -0,0 +1,20 @@
+package com.masterjedi;
+
+public class DooBee {
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        }
+
+        if (x == 3) {
+            System.out.print("Do");
+        }
+    }
+}
+
+
